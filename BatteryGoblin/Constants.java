82b324a68c910418371ef66b170eb8345bc99fef
MonkeyMike99 <105067938+MonkeyMike99@users.noreply.github.com>
2022-05-06 09:04:51 -0700

    Update Constants.java

--- BatteryGoblin/Constants.java
+++ BatteryGoblin/Constants.java
@@ -12,16 +12,17 @@ public class Constants
     //WORLD_WIDTH and WORLD_HEIGHT proportional to config.width and config.height in GameLauncher class
     public static final float WORLD_WIDTH = 800; 
     public static final float WORLD_HEIGHT = 480;
-
+    
     //other constants we will need
     public static final float BALL_SPEED = 7;
     public static final float BULLET_RADIUS = 7;
     public static final float BULLET_SPEED = 20;
     
-    public static final int TOP = 1; 
-    public static final int BOTTOM = 2; 
-    public static final int LEFT = 3; 
-    public static final int RIGHT = 4; 
+    public static final int TOP = 0; 
+    public static final int BOTTOM = 1; 
+    public static final int LEFT = 2; 
+    public static final int RIGHT = 3; 
     
       public static final float MAX_HEALTH = 100;
+    
 }
