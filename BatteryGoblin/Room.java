15af92360480ee2056861bf9a8264ed345b97851
MonkeyMike99 <105067938+MonkeyMike99@users.noreply.github.com>
2022-05-06 08:57:53 -0700

    Final Project

---
+++ BatteryGoblin/Room.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class Room here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Room
+{
+    private boolean hasLeftDoor;
+    private boolean hasRightDoor;
+    private boolean hasTopDoor;
+    private boolean hasBotDoor;
+    private 
+    
+    public Room(boolean left,boolean right,boolean top,boolean bot){
+        hasLeftDoor=left;
+        hasRightDoor=right;
+        hasTopDoor=top;
+        hasBotDoor=bot;
+    }
+    
+    
+}
